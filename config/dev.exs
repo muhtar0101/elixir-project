60e37d04261d8ac172db89e9ca3c69268ce2c27a
Mukhtar <muhtar0101@gmail.com>
2025-08-13 17:24:29 +0500

    auto: 2025-08-13 17:24:29

--- config/dev.exs
+++ config/dev.exs
@@ -1,26 +1,21 @@
 # config/dev.exs
 import Config
 
-config :lms, Lms.Repo,
-  url: System.get_env("DATABASE_URL") || "ecto://postgres:postgres@db:5432/lms_dev",
-  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
-  show_sensitive_data_on_connection_error: true,
-  stacktrace: true
+port = String.to_integer(System.get_env("PORT") || "4000")
+host = System.get_env("PHX_HOST") || "localhost"
 
 config :lms, LmsWeb.Endpoint,
-  http: [ip: {0,0,0,0}, port: String.to_integer(System.get_env("PORT") || "4000")],
+  http: [ip: {0, 0, 0, 0}, port: port],
+  url: [host: host, port: port],
   check_origin: false,
   code_reloader: true,
   debug_errors: true,
-  secret_key_base: System.get_env("SECRET_KEY_BASE") || "dev-key"
-
-config :logger, :console, format: "[$level] $message\n"
-
-# config/dev.exs
-config :lms, LmsWeb.Endpoint,
+  secret_key_base: System.get_env("SECRET_KEY_BASE") || "dev-only-secret-please-change",
   watchers: [
     esbuild: {Esbuild, :install_and_run, [:default, ~w(--sourcemap=inline --watch)]},
     tailwind: {Tailwind, :install_and_run, [:default, ~w(--watch)]}
   ]
-# config/dev.exs
-#config :lms, Lms.Mailer, adapter: Swoosh.Adapters.Local
+
+config :logger, :console, format: "[$level] $message\n"
+config :phoenix, :stacktrace_depth, 20
+config :phoenix, :plug_init_mode, :runtime
