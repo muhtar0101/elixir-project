9f5680c9a449bb2f9fe7cd298fac46f91be10fac
Mukhtar <muhtar0101@gmail.com>
2025-08-13 15:47:21 +0500

    auto: 2025-08-13 15:47:21

--- lib/lms_web/router.ex
+++ lib/lms_web/router.ex
@@ -1,42 +1,50 @@
-import LmsWeb.UserAuth
 defmodule LmsWeb.Router do
   use LmsWeb, :router
+  import LmsWeb.UserAuth
 
-  import Phoenix.LiveView.Router
-
-  # Егер phx.gen.auth арқылы UserAuth модулі бар болса:
-  # plug-тарды осылай қосамыз (fetch_current_user керек)
   pipeline :browser do
-  plug :accepts, ["html"]
-  plug :fetch_session
-  plug :fetch_live_flash
-  plug :put_root_layout, {LmsWeb.Layouts, :root}
-  plug :protect_from_forgery
-  plug :put_secure_browser_headers
-  plug :fetch_current_user   # <<< Назар: импортталған функция ретінде шақырамыз
-end
+    plug :accepts, ["html"]
+    plug :fetch_session
+    plug :fetch_live_flash
+    plug :put_root_layout, {LmsWeb.Layouts, :root}
+    plug :protect_from_forgery
+    plug :put_secure_browser_headers
+    plug :fetch_current_user
+  end
 
-  pipeline :admin do
-    plug LmsWeb.Plugs.RequireAdmin
+  pipeline :api do
+    plug :accepts, ["json"]
   end
 
   scope "/", LmsWeb do
     pipe_through :browser
 
-    # әдеттегі беттеріңіз...
-
-    live "/settings", SettingsLive, :index
-  end
-
-  scope "/admin", LmsWeb do
-    pipe_through :browser
+    # Аутентификация
     get   "/login",  AuthController, :login_form
     post  "/login",  AuthController, :login
     get   "/logout", AuthController, :logout
     delete "/logout", AuthController, :logout
-    live "/users", Admin.UsersLive, :index
-    live "/users/:id/edit", Admin.UserEditLive, :edit
+
+    # Тіркелу
+    get  "/register", RegistrationController, :new
+    post "/register", RegistrationController, :create
+
+    # Сатып алу
+    post "/purchase/:slug", PurchaseController, :create
+
+    # Қонақ беттері
     live "/", HomeLive, :index
     live "/courses/:slug", CourseShowLive, :show
+    live "/courses/:slug/lessons/:id", LessonShowLive, :show
+  end
+
+  scope "/admin", LmsWeb.Admin do
+    pipe_through :browser
+    plug :require_admin
+
+    live "/users", UsersLive, :index
+    live "/users/:id/edit", UserEditLive, :edit
+    live "/courses/:id/edit", CourseEditorLive, :edit
+    # Қажетіне қарай admin роуттарын осы жерге қосамыз
   end
 end
