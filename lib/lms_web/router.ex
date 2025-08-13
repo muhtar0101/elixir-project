9723796a743c949368f5dff900091fedd8217044
Mukhtar <muhtar0101@gmail.com>
2025-08-13 16:19:40 +0500

    auto: 2025-08-13 16:19:40

--- lib/lms_web/router.ex
+++ lib/lms_web/router.ex
@@ -2,9 +2,8 @@ defmodule LmsWeb.Router do
   use LmsWeb, :router
 
   import Phoenix.LiveView.Router
+  import LmsWeb.UserAuth   # require_authenticated_user/2, require_admin/2, fetch_current_user/2 т.б.
 
-  # Егер phx.gen.auth арқылы UserAuth модулі бар болса:
-  # plug-тарды осылай қосамыз (fetch_current_user керек)
   pipeline :browser do
     plug :accepts, ["html"]
     plug :fetch_session
@@ -12,25 +11,48 @@ defmodule LmsWeb.Router do
     plug :put_root_layout, {LmsWeb.Layouts, :root}
     plug :protect_from_forgery
     plug :put_secure_browser_headers
-    plug LmsWeb.UserAuth, :fetch_current_user
+    plug :fetch_current_user   # НАЗАР АУДАРЫҢЫЗ: plug pipeline ішінде осылай шақырылады
   end
 
   pipeline :admin do
-    plug LmsWeb.Plugs.RequireAdmin
+    plug :require_admin
   end
 
   scope "/", LmsWeb do
     pipe_through :browser
 
-    # әдеттегі беттеріңіз...
+    # Басты бет
+    live "/", HomeLive, :index
 
+    # Аутентификация (контроллерлеріңіз бар деп есептеймін)
+    get  "/login",     AuthController, :new
+    post "/login",     AuthController, :create
+    get  "/logout",    AuthController, :delete
+
+    get  "/register",  RegistrationController, :new
+    post "/register",  RegistrationController, :create
+
+    # Курстар (LiveView беттеріңізде қолданылып тұр)
+    live "/courses/:slug", CourseShowLive, :show
+    post "/purchase/:slug", PurchaseController, :create
+
+    # Пайдаланушының өз параметрлері
     live "/settings", SettingsLive, :index
   end
 
   scope "/admin", LmsWeb do
     pipe_through [:browser, :admin]
 
-    live "/users", Admin.UsersLive, :index
+    # Админ: Қолданушылар
+    live "/users",        Admin.UsersLive, :index
     live "/users/:id/edit", Admin.UserEditLive, :edit
+
+    # Қаласаңыз: Курстар редакторы (кодта сілтеме бар, сондықтан маршрут беріп қояйық)
+    live "/courses/:id", Admin.CourseEditorLive, :index
+
+    # Сабақтарды өңдеу сілтемесі кодта бар екен, уақытша плейсхолдер маршрут
+    # Егер нақты LiveView кейін жасайтын болсаңыз да, осы маршрут болғаны
+    # ~p"/admin/courses/#{@course.id}/lessons/#{l.id}/edit" ескертуін алып тастайды.
+    live "/courses/:course_id/lessons/:id/edit", Admin.LessonEditLive, :edit
   end
 end
